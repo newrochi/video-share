1fdfda786a458022f0ce46373e824409695848c0
newrochi <yusufyuzlu2008@gmail.com>
2021-12-14 04:08:40 -0400

    video accessor created

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Hekmatinasser\Verta\Verta;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
     use HasFactory;
+
+    public function getLengthAttribute($value){
+        return gmdate("i:s",$value);
+    }
+    public function getCreatedAtAttribute($value){
+        return (new Verta($value))->formatDifference();
+    }
 }
