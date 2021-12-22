3490f6beb23267730eadfbf0b38498cd87fb8eb5
newrochi <yusufyuzlu2008@gmail.com>
2021-12-22 00:18:05 -0400

    prepared inputs for validation

--- app/Http/Requests/StoreVideoRequest.php
+++ app/Http/Requests/StoreVideoRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 
 class StoreVideoRequest extends FormRequest
 {
@@ -26,9 +27,16 @@ class StoreVideoRequest extends FormRequest
         return [
             'name'=>['required'],
             'length'=>['required','integer'],
-            'slug'=>['required','unique:videos,slug'],
+            'slug'=>['required','unique:videos,slug','alpha_dash'],
             'url'=>['required','url'],
             'thumbnail'=>['required','url']
         ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'slug' => Str::slug($this->slug),
+        ]);
     }
 }
